185edca6dcf67c05f760aa0b62217e6169db7a70
“Aline <hy.aline@outlook.com>
2024-03-10 23:36:04 +0100

    added styleguide into a stylesheet.css with variables, need some testing, adding buttons and added function to add stylesheet css into the wordpress theme in the funtions.php

---
+++ header-test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>style.css" type="text/css" />
+    <title><?php bloginfo('name'); ?></title>
+
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <main>
+        <div class="button-container-test">
+            <a href="" class="button button--white">Hello</a>
+        </div>
+        <blockquote>“The first rule of any organic used in a business is that nature applied to an efficient operation will magnify the efficiency. The second is that organic applied to an inefficient operation will magnify the health.”</blockquote>
+    </main>
+</body>
