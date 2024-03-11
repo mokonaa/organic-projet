24c86d84ba3d51d21f30a515c7f2d8ac4942cec1
Maria <mariaguydefontgalland@gmail.com>
2024-03-11 22:45:00 +0100

    (feat:) implémentation du style guide pour le banner

--- banner.php
+++ banner.php
@@ -7,7 +7,7 @@ if (get_theme_mod('banner_image') && get_theme_mod('banner_title') && get_theme_
             <h1 class="banner__titre">
                 <?php echo get_theme_mod('banner_title') ? get_theme_mod('banner_title') : 'Welcome'; ?>
             </h1>
-            <button class="banner__button"><a class="banner__buttonUrl" target="_blank" href="<?php echo get_theme_mod('banner_url') ?>" >Explore now </a></button>
+            <button class="banner__button button button--yellow "><a class="banner__buttonUrl" target="_blank" href="<?php echo get_theme_mod('banner_url') ?>" >Explore now </a></button>
         </div>
     </div>
 <?php 
