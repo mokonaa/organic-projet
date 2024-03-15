15441094e9abf98ebda0df176e81b5d1febdb424
Maria <mariaguydefontgalland@gmail.com>
2024-03-15 21:38:07 +0100

    (feat:) ajout font family yellowtail (import & variable)

--- banner.php
+++ banner.php
@@ -7,11 +7,7 @@ if (get_theme_mod('banner_image') && get_theme_mod('banner_title') && get_theme_
             <h1 class="banner__titre">
                 <?php echo get_theme_mod('banner_title') ? get_theme_mod('banner_title') : 'Welcome'; ?>
             </h1>
-<<<<<<< HEAD
             <button class="banner__button button button--yellow "><a class="banner__buttonUrl" target="_blank" href="<?php echo get_theme_mod('banner_url') ?>" ><?php echo get_theme_mod('banner_urlTxt') ? get_theme_mod('banner_urlTxt') : 'Explore'; ?> </a></button>
-=======
-            <button class="banner__button"><a class="banner__buttonUrl" target="_blank" href="<?php echo get_theme_mod('banner_url') ?>" >Explore now </a></button>
->>>>>>> df6bfff ((feat:) implémentation d'un banner modifiable depuis l'admin de wordpress pour la page d'accueil)
         </div>
     </div>
 <?php 
--- promotions.php
+++ promotions.php
@@ -1,23 +1,23 @@
 <?php 
 if (get_theme_mod('promotion_title0') && get_theme_mod('promotion_desc0')){ ?>
     <div class="promotionsList">
-        <div class="promotion">
+        <div class="promotion promotion_0">
             <img class="promotion__img" src="<?php echo get_theme_mod('promotion_image0') ?>" />
             <div class="promotion__partieTextes">
                 <p class="promotion__description"><?php echo get_theme_mod('promotion_desc0') ?></p>
-                <h1 class="promotion__titre">
+                <p class="promotion__titre">
                     <?php echo get_theme_mod('promotion_title0') ? get_theme_mod('promotion_title0') : 'Welcome'; ?>
-                </h1>
+                </p>
             </div>
         </div>
 
-        <div class="promotion">
+        <div class="promotion promotion_1">
             <img class="promotion__img" src="<?php echo get_theme_mod('promotion_image1') ?>" />
             <div class="promotion__partieTextes">
                 <p class="promotion__description"><?php echo get_theme_mod('promotion_desc1') ?></p>
-                <h1 class="promotion__titre">
+                <p class="promotion__titre">
                     <?php echo get_theme_mod('promotion_title1') ? get_theme_mod('promotion_title1') : 'Welcome'; ?>
-                </h1>
+                </p>
             </div>
         </div>
     </div>
