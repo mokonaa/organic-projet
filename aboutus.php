df8c03a51bd29b5fbdf8bb5ae88f4700fc3ff338
Yolène <yolene@brokeandabroad.com>
2024-03-18 00:33:34 +0100

    fix HTML in about us section

--- aboutus.php
+++ aboutus.php
@@ -7,23 +7,22 @@ if (get_theme_mod('aboutus_title') && get_theme_mod('aboutus_description') && ge
         <div class="aboutusPartieDroite">
             <div class="aboutusPartieDroite__partieTextes">
                 <p class="aboutusPartieDroite__description"><?php echo get_theme_mod('aboutus_description') ?></p>
-                <h1 class="aboutusPartieDroite__titre">
+                <h2 class="aboutusPartieDroite__titre">
                     <?php echo get_theme_mod('aboutus_title') ? get_theme_mod('aboutus_title') : 'Welcome'; ?>
-                </h1>
+                </h2>
                 <p class="aboutusPartieDroite__paragraphe"> <?php echo get_theme_mod('aboutus_text') ?></p>
-
                 <div>
                     <div class="aboutusPartieDroite__bloc">
                         <div class="aboutusPartieDroite__blocimg"><img src="<?php echo get_theme_mod('aboutusbloc1_image') ?>"/></div>
                         <div>
-                            <h1 class="aboutusPartieDroite__blocsTitre"><?php echo get_theme_mod('aboutus_bloc1title') ?></p>
+                            <h6 class="aboutusPartieDroite__blocsTitre"><?php echo get_theme_mod('aboutus_bloc1title') ?></h6>
                             <p class="aboutusPartieDroite__blocsDescription"><?php echo get_theme_mod('aboutus_bloc1description') ?></p>
                         </div>
                     </div>
                     <div class="aboutusPartieDroite__bloc">
                         <div class="aboutusPartieDroite__blocimg"><img src="<?php echo get_theme_mod('aboutusbloc2_image') ?>"/></div>
                         <div>
-                            <h1 class="aboutusPartieDroite__blocsTitre"><?php echo get_theme_mod('aboutus_bloc2title') ?></p>
+                            <h6 class="aboutusPartieDroite__blocsTitre"><?php echo get_theme_mod('aboutus_bloc2title') ?></h6>
                             <p class="aboutusPartieDroite__blocsDescription"><?php echo get_theme_mod('aboutus_bloc2description') ?></p>
                         </div>
                     </div>
